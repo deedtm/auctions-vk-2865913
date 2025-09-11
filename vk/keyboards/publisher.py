0a93915ed8c2e50c15c346f0eccb1658bf2d0464
deedtm <misha.nikto000@gmail.com>
2025-09-11 21:38:29 +0400

    fix for overlimited

--- vk/keyboards/publisher.py
+++ vk/keyboards/publisher.py
@@ -4,7 +4,7 @@ from vkbottle import Keyboard, Callback
 
 def overlimit_kb(with_other_group: bool):
     overlimit_kb = Keyboard(inline=True)
-    overlimit_kb.add(Callback(PUBLISHER["same_group"], {"overlimit": "same_group"}))
+    overlimit_kb.add(Callback(PUBLISHER["same_group"], {"overlimited": "same_group"}))
     if with_other_group:
-        overlimit_kb.add(Callback(PUBLISHER["other_group"], {"overlimit": "other_group"}))
+        overlimit_kb.add(Callback(PUBLISHER["other_group"], {"overlimited": "other_group"}))
     return overlimit_kb
